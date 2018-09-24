1aa1870b08707c4592eb849206aea64cae13f33b
Seppe Snoeck <seppe.snoeck@student.odisee.be>
2018-09-24 16:41:43 +0200

    Add product should be a pure component

--- src/Components/products/AddProduct.js
+++ src/Components/products/AddProduct.js
@@ -1,7 +1,7 @@
-import React, { Component, Fragment } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-class AddProduct extends Component {
+class AddProduct extends PureComponent {
   constructor(props) {
     super(props);
     const { products } = this.props;
